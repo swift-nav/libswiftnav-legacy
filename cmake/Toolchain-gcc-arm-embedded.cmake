b3e95df3f5f8719a83f8cb3714f2d10fdf04a9ae
Fergus Noble <fergusnoble@gmail.com>
2013-02-05 13:31:32 -0800

    Turbo speed! Enable optimisations on x86 family CPUs.
    Also updating copyright notices on CMake scripts.

--- cmake/Toolchain-gcc-arm-embedded.cmake
+++ cmake/Toolchain-gcc-arm-embedded.cmake
@@ -1,5 +1,15 @@
 # CMake Toolchain file for the gcc-arm-embedded toolchain.
 # https://launchpad.net/gcc-arm-embedded
+#
+# Copyright (c) 2013 Swift Navigation Inc.
+# Contact: Fergus Noble <fergus@swift-nav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 
 include(CMakeForceCompiler)
 
