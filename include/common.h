8ab443d2c801480958b434d9f179e57c916f9f4f
Fergus Noble <fergusnoble@gmail.com>
2013-07-05 23:32:23 -0700

    locm3 now no longer defines the short type names.

--- include/common.h
+++ include/common.h
@@ -18,34 +18,32 @@
  * Common definitions used throughout the library.
  * \{ */
 
-#ifndef LIBOPENCM3_CM3_COMMON_H
-  #include <stdint.h>
-  #include <stdbool.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-  /** \defgroup common_inttypes Integer types
-   * Specified-width integer type definitions for shorter and nicer code.
-   *
-   * These should be used in preference to unspecified width types such as
-   * `int` which can lead to portability issues between different platforms.
-   * \{ */
+/** \defgroup common_inttypes Integer types
+ * Specified-width integer type definitions for shorter and nicer code.
+ *
+ * These should be used in preference to unspecified width types such as
+ * `int` which can lead to portability issues between different platforms.
+ * \{ */
 
-  /** Signed 8-bit integer. */
-  typedef int8_t s8;
-  /** Signed 16-bit integer. */
-  typedef int16_t s16;
-  /** Signed 32-bit integer. */
-  typedef int32_t s32;
-  /** Unsigned 8-bit integer. */
-  typedef uint8_t u8;
-  /** Unsigned 16-bit integer. */
-  typedef uint16_t u16;
-  /** Unsigned 32-bit integer. */
-  typedef uint32_t u32;
-  /** Unsigned 64-bit integer. */
-  typedef uint64_t u64;
+/** Signed 8-bit integer. */
+typedef int8_t s8;
+/** Signed 16-bit integer. */
+typedef int16_t s16;
+/** Signed 32-bit integer. */
+typedef int32_t s32;
+/** Unsigned 8-bit integer. */
+typedef uint8_t u8;
+/** Unsigned 16-bit integer. */
+typedef uint16_t u16;
+/** Unsigned 32-bit integer. */
+typedef uint32_t u32;
+/** Unsigned 64-bit integer. */
+typedef uint64_t u64;
 
-  /** \} */
-#endif
+/** \} */
 
 /** \} */
 
