53e4285c871ee1ddc960f7ce65faeb8a11f5512f
Fergus Noble <fergusnoble@gmail.com>
2014-01-24 20:51:17 -0800

    SBP - unit tests for send_message

--- tests/check_sbp.c
+++ tests/check_sbp.c
@@ -10,6 +10,12 @@ u32 dummy_wr = 0;
 u32 dummy_rd = 0;
 u8 dummy_buff[1024];
 
+void dummy_reset()
+{
+  dummy_rd = dummy_wr = 0;
+  memset(dummy_buff, 0, sizeof(dummy_buff));
+}
+
 u32 dummy_write(u8 *buff, u32 n)
 {
  u32 real_n = n;//(dummy_n > n) ? n : dummy_n;
@@ -32,46 +38,60 @@ typedef struct {
 void printy_callback(u16 sender_id, u8 len, u8 msg[])
 {
   printf("MSG id: 0x%04X, len: %d\n", sender_id, len);
-  for (u8 i=0; i<len; i++)
-    printf("0x%02X ", msg[i]);
+  if (len > 0) {
+    for (u8 i=0; i<len; i++)
+      printf("0x%02X ", msg[i]);
+  }
   printf("\n");
 }
 
-void test_callback(u16 sender_id __attribute__((unused)),
-                   u8 len __attribute__((unused)),
-                   u8 msg[] __attribute__((unused)))
+void test_callback(u16 sender_id, u8 len, u8 msg[])
 {
   /* Do nothing. */
+  (void)sender_id;
+  (void)len;
+  (void)msg;
 }
-void test_callback2(u16 sender_id __attribute__((unused)),
-                    u8 len __attribute__((unused)),
-                    u8 msg[] __attribute__((unused)))
+void test_callback2(u16 sender_id, u8 len, u8 msg[])
 {
   /* Do nothing. */
+  (void)sender_id;
+  (void)len;
+  (void)msg;
 }
 
 START_TEST(test_sbp_send_message)
 {
   awesome_struct s = { 0x22, 0x33 };
 
-  sbp_send_message(0x4455, 0x6677, sizeof(s), (u8*)&s, &dummy_write);
+  fail_unless(sbp_send_message(0x2233, 0x4455, 0, (u8*)&s, 0) == SBP_NULL_ERROR,
+      "sbp_send_message should return an error if write is NULL");
 
-  static sbp_msg_callbacks_node_t test_node;
-  sbp_register_callback(0x4456, &printy_callback, &test_node);
+  dummy_reset();
+  fail_unless(sbp_send_message(0x2233, 0x4455, 1, 0, &dummy_write)
+        == SBP_NULL_ERROR,
+      "sbp_send_message should return an error if payload is NULL and len != 0");
 
-  sbp_state_t state;
-  sbp_state_init(&state);
-  s8 ret;
-  while (dummy_wr > dummy_rd) {
-    if ((ret = sbp_process(&state, &dummy_read)) != SBP_OK)
-      printf("Uh oh! %d\n", ret);
-  }
+  dummy_reset();
+  fail_unless(sbp_send_message(0x2233, 0x4455, 0, 0, &dummy_write)
+        == SBP_OK,
+      "sbp_send_message should return OK if payload is NULL and len == 0");
 
-  /*
-  ret = getbitu(test_data, 10, 13);
-  fail_unless(ret == 0x11A2,
-      "test case 6 expected 0x11A2, got 0x%04X", ret);
-  */
+  u8 zero_len_message[] = {0x55, 0x33, 0x22, 0x55, 0x44, 0x0, 0x36, 0x74};
+
+  fail_unless(memcmp(dummy_buff, zero_len_message, sizeof(zero_len_message))
+        == 0,
+      "sbp_send_message encode error for len = 0");
+
+  dummy_reset();
+  sbp_send_message(0x2233, 0x6677, sizeof(s), (u8*)&s, &dummy_write);
+
+  u8 awesome_message[] = {0x55, 0x33, 0x22, 0x77, 0x66,
+                          0x02, 0x22, 0x33, 0xCD, 0x0C};
+
+  fail_unless(memcmp(dummy_buff, awesome_message, sizeof(awesome_message))
+        == 0,
+      "sbp_send_message encode error for test message");
 }
 END_TEST
 
@@ -141,8 +161,6 @@ END_TEST
 
 Suite* sbp_suite(void)
 {
-  (void)test_sbp_send_message;
-
   Suite *s = suite_create("SBP");
 
   TCase *tc_core = tcase_create("Core");
@@ -151,7 +169,7 @@ Suite* sbp_suite(void)
   tcase_add_checked_fixture(tc_core, sbp_clear_callbacks, sbp_clear_callbacks);
 
   tcase_add_test(tc_core, test_callbacks);
-  /*tcase_add_test(tc_core, test_sbp_send_message);*/
+  tcase_add_test(tc_core, test_sbp_send_message);
 
   suite_add_tcase(s, tc_core);
 
