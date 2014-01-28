92f477863c88849d20745308aef279134acd5337
Fergus Noble <fergusnoble@gmail.com>
2014-01-28 12:00:34 -0800

    Initial commit for SBP message header generation code.

---
+++ sbp_generate/sbp_messages_template.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2013 Swift Navigation Inc.
+ * Contact: Fergus Noble <fergus@swift-nav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*****************************************************************************
+ * Automatically generated from sbp.yaml with generate.py, do not hand edit! *
+ *****************************************************************************/
+
+#ifndef LIBSWIFTNAV_SBP_H
+#define LIBSWIFTNAV_SBP_H
+
+#include "common.h"
+
+((* for m in msgs *))
+/** (((m.short_desc)))
+(((m.desc|commentify)))
+ */
+#define SBP_(((m.name))) ((('0x%04X'|format(m.id))))
+
+typedef struct __attribute__((packed)) {
+((*- for f in m.fields *))
+  (((f.type))) (((f.name))); /**< (((f.desc))) */
+((*- endfor *))
+} sbp_(((m.name|lower)))_t;
+
+((* endfor *))
+#endif /* LIBSWIFTNAV_SBP_H */
+
+
