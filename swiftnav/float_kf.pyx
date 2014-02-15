2ad653a47aff54dd67c5977fca868c8693b9bd57
Ian Horn <horn.imh@gmail.com>
2014-02-14 18:49:56 -0800

    using C observation updates

--- swiftnav/float_kf.pyx
+++ swiftnav/float_kf.pyx
@@ -19,7 +19,7 @@ def udu(M):
     np.empty((n,n), dtype=np.double)
   cdef np.ndarray[np.double_t, ndim=1, mode="c"] D = \
     np.empty(n, dtype=np.double)
-  float_kf_c.udu(n, <double **> &M_[0,0], <double **> &U[0,0], <double *> &D[0])
+  float_kf_c.udu(n, <double *> &M_[0,0], <double *> &U[0,0], <double *> &D[0])
   return UDU_decomposition(U, D)
 
 def reconstruct_udu(ud):
@@ -33,10 +33,82 @@ def reconstruct_udu(ud):
   cdef np.ndarray[np.double_t, ndim=2, mode="c"] M = \
     np.empty((n,n), dtype=np.double)
 
-  float_kf_c.reconstruct_udu(n, <double **> &U[0,0], <double *> &D[0], <double **> &M[0,0])
+  float_kf_c.reconstruct_udu(n, <double *> &U[0,0], <double *> &D[0], <double *> &M[0,0])
   return M
 
+def predict_forward(transition_mtx, transition_cov, state_mean, state_cov_UDU):
+  state_dim = transition_mtx.shape[0]
 
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] transition_mtx_ = \
+    np.array(transition_mtx, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] transition_cov_ = \
+    np.array(transition_cov, dtype=np.double)
+
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] state_mean_ = \
+    np.array(state_mean, dtype=np.double)
+
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] state_cov_U_ = \
+    np.array(state_cov_UDU.U, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] state_cov_D_ = \
+    np.array(state_cov_UDU.D, dtype=np.double)
+
+  float_kf_c.predict_forward(state_dim,
+                             <double *> &transition_mtx_[0,0], 
+                             <double *> &transition_cov_[0,0], 
+                             <double *> &state_mean_[0], 
+                             <double *> &state_cov_U_[0,0], 
+                             <double *> &state_cov_D_[0])
+  return state_mean_, UDU_decomposition(state_cov_U_, state_cov_D_)
+
+def update_scalar_measurement(h, R, U, D):
+  state_dim = h.shape[0]
+
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] h_ = \
+    np.array(h, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] U_ = \
+    np.array(U, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] D_ = \
+    np.array(D, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] k = \
+    np.empty(state_dim, dtype=np.double)
+  # cdef np.ndarray[np.double_t, ndim=1, mode="c"] R_ = \
+  #   np.array([R], dtype=np.double)
+
+  float_kf_c.update_scalar_measurement(state_dim,
+                                       <double *> &h_[0],
+                                       <double> R,
+                                       <double *> &U_[0,0],
+                                       <double *> &D_[0],
+                                       <double *> &k[0])
+  return UDU_decomposition(U_, D_), k
+
+def update_for_obs(decor_obs_mtx, decor_obs_cov,
+                   intermediate_mean, intermediate_cov_U, intermediate_cov_D,
+                   decor_obs):
+  state_dim = intermediate_cov_D.shape[0]
+  obs_dim = decor_obs.shape[0]
+
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] decor_obs_mtx_ = \
+    np.array(decor_obs_mtx, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] decor_obs_cov_ = \
+    np.array(decor_obs_cov, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] intermediate_mean_ = \
+    np.array(intermediate_mean, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] intermediate_cov_U_ = \
+    np.array(intermediate_cov_U, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] intermediate_cov_D_ = \
+    np.array(intermediate_cov_D, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] decor_obs_ =  \
+    np.array(decor_obs, dtype=np.double)
+
+  float_kf_c.update_for_obs(state_dim, obs_dim,
+                            <double *> &decor_obs_mtx_[0,0],
+                            <double *> &decor_obs_cov_[0], 
+                            <double *> &intermediate_mean_[0], 
+                            <double *> &intermediate_cov_U_[0,0], 
+                            <double *> &intermediate_cov_D_[0],
+                            <double *> &decor_obs_[0])
+  return intermediate_mean_, UDU_decomposition(intermediate_cov_U_, intermediate_cov_D_)
 
 class UDU_decomposition:
   def __init__(self, U, D):
--- swiftnav/float_kf_c.pxd
+++ swiftnav/float_kf_c.pxd
@@ -10,7 +10,14 @@
 from common cimport *
 
 cdef extern from "libswiftnav/float_kf.h":
-  s8 udu(u32 n, double *M[], double *U[], double *D)
-  void triu(u32 n, double *M[])
-  void eye(u32 n, double *M[])
-  void reconstruct_udu(u32 n, double *U[], double *D, double *M[])
+  s8 udu(u32 n, double *M, double *U, double *D)
+  void triu(u32 n, double *M)
+  void eye(u32 n, double *M)
+  void reconstruct_udu(u32 n, double *U, double *D, double *M)
+  void predict_forward(u32 state_dim, double *transition_mtx, double *transition_cov, double *state_mean, double *state_cov_U, double *state_cov_D)
+  void update_scalar_measurement(u32 state_dim, double *h, double R,
+                               double *U, double *D, double *k)
+  void update_for_obs(u32 state_dim, u32 obs_dim, double *decor_obs_mtx, double *decor_obs_cov, 
+                    double *intermediate_mean, double *intermediate_cov_U, double *intermediate_cov_D,
+                    double *decor_obs)
+
