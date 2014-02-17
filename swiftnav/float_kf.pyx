174d5b874bd4b40305b0db2439933d7dc26562d6
Ian Horn <horn.imh@gmail.com>
2014-02-17 15:03:55 -0800

    added struct for KF

--- swiftnav/float_kf.pyx
+++ swiftnav/float_kf.pyx
@@ -10,6 +10,7 @@
 import numpy as np
 cimport numpy as np
 cimport float_kf_c
+from libc.string cimport memcpy
 
 def udu(M):
   n = M.shape[0]
@@ -36,14 +37,7 @@ def reconstruct_udu(ud):
   float_kf_c.reconstruct_udu(n, <double *> &U[0,0], <double *> &D[0], <double *> &M[0,0])
   return M
 
-def predict_forward(transition_mtx, transition_cov, state_mean, state_cov_UDU):
-  state_dim = transition_mtx.shape[0]
-
-  cdef np.ndarray[np.double_t, ndim=2, mode="c"] transition_mtx_ = \
-    np.array(transition_mtx, dtype=np.double)
-  cdef np.ndarray[np.double_t, ndim=2, mode="c"] transition_cov_ = \
-    np.array(transition_cov, dtype=np.double)
-
+def predict_forward(KalmanFilter kf, state_mean, state_cov_UDU):
   cdef np.ndarray[np.double_t, ndim=1, mode="c"] state_mean_ = \
     np.array(state_mean, dtype=np.double)
 
@@ -52,9 +46,7 @@ def predict_forward(transition_mtx, transition_cov, state_mean, state_cov_UDU):
   cdef np.ndarray[np.double_t, ndim=1, mode="c"] state_cov_D_ = \
     np.array(state_cov_UDU.D, dtype=np.double)
 
-  float_kf_c.predict_forward(state_dim,
-                             <double *> &transition_mtx_[0,0], 
-                             <double *> &transition_cov_[0,0], 
+  float_kf_c.predict_forward(&(kf.kf),
                              <double *> &state_mean_[0], 
                              <double *> &state_cov_U_[0,0], 
                              <double *> &state_cov_D_[0])
@@ -117,3 +109,79 @@ class UDU_decomposition:
   def reconstruct(self):
     return reconstruct_udu(self)
 
+cdef class KalmanFilter:
+  cdef float_kf_c.kf_t kf
+  def __init__(self, 
+               np.ndarray[np.double_t, ndim=2, mode="c"] transition_mtx,
+               np.ndarray[np.double_t, ndim=2, mode="c"] transition_cov,
+               np.ndarray[np.double_t, ndim=2, mode="c"] obs_cov_root_inv, 
+               np.ndarray[np.double_t, ndim=2, mode="c"] decor_obs_mtx, 
+               np.ndarray[np.double_t, ndim=1, mode="c"] decor_obs_cov):
+    self.state_dim = transition_mtx.shape[0]
+    self.obs_dim = obs_cov_root_inv.shape[0]
+    self.transition_mtx = transition_mtx
+    self.transition_cov = transition_cov
+    self.obs_cov_root_inv = obs_cov_root_inv
+    self.decor_obs_mtx = decor_obs_mtx
+    self.decor_obs_cov = decor_obs_cov
+
+  def __repr__(self):
+    return "<KalmanFilter with state_dim=" + str(self.state_dim) + \
+           ", obs_dim=" + str(self.obs_dim) + ">"
+
+  property state_dim:
+    def __get__(self):
+      return self.kf.state_dim
+    def __set__(self, state_dim):
+      self.kf.state_dim = state_dim
+
+  property obs_dim:
+    def __get__(self):
+      return self.kf.obs_dim
+    def __set__(self, obs_dim):
+      self.kf.obs_dim = obs_dim
+
+  property transition_mtx:
+    def __get__(self):
+      cdef np.ndarray[np.double_t, ndim=2, mode="c"] transition_mtx = \
+        np.empty((self.state_dim, self.state_dim), dtype=np.double)
+      memcpy(&transition_mtx[0,0], self.kf.transition_mtx, self.state_dim * self.state_dim * sizeof(double))
+      return transition_mtx
+    def __set__(self, np.ndarray[np.double_t, ndim=2, mode="c"] transition_mtx):
+      memcpy(self.kf.transition_mtx, &transition_mtx[0,0], self.state_dim * self.state_dim * sizeof(double))
+
+  property transition_cov:
+    def __get__(self):
+      cdef np.ndarray[np.double_t, ndim=2, mode="c"] transition_cov = \
+        np.empty((self.state_dim, self.state_dim), dtype=np.double)
+      memcpy(&transition_cov[0,0], self.kf.transition_cov, self.state_dim * self.state_dim * sizeof(double))
+      return transition_cov
+    def __set__(self, np.ndarray[np.double_t, ndim=2, mode="c"] transition_cov):
+      memcpy(self.kf.transition_cov, &transition_cov[0,0], self.state_dim * self.state_dim * sizeof(double))
+
+  property obs_cov_root_inv:
+    def __get__(self):
+      cdef np.ndarray[np.double_t, ndim=2, mode="c"] obs_cov_root_inv = \
+        np.empty((self.obs_dim, self.obs_dim), dtype=np.double)
+      memcpy(&obs_cov_root_inv[0,0], self.kf.obs_cov_root_inv, self.obs_dim * self.obs_dim * sizeof(double))
+      return obs_cov_root_inv
+    def __set__(self, np.ndarray[np.double_t, ndim=2, mode="c"] obs_cov_root_inv):
+      memcpy(self.kf.obs_cov_root_inv, &obs_cov_root_inv[0,0], self.obs_dim * self.obs_dim * sizeof(double))
+
+  property decor_obs_mtx:
+    def __get__(self):
+      cdef np.ndarray[np.double_t, ndim=2, mode="c"] decor_obs_mtx = \
+        np.empty((self.obs_dim, self.obs_dim), dtype=np.double)
+      memcpy(&decor_obs_mtx[0,0], self.kf.decor_obs_mtx, self.obs_dim * self.obs_dim * sizeof(double))
+      return decor_obs_mtx
+    def __set__(self, np.ndarray[np.double_t, ndim=2, mode="c"] decor_obs_mtx):
+      memcpy(self.kf.decor_obs_mtx, &decor_obs_mtx[0,0], self.obs_dim * self.obs_dim * sizeof(double))
+
+  property decor_obs_cov:
+    def __get__(self):
+      cdef np.ndarray[np.double_t, ndim=1, mode="c"] decor_obs_cov = \
+        np.empty(self.obs_dim, dtype=np.double)
+      memcpy(&decor_obs_cov[0], self.kf.decor_obs_cov, self.obs_dim * sizeof(double))
+      return decor_obs_cov
+    def __set__(self, np.ndarray[np.double_t, ndim=1, mode="c"] decor_obs_cov):
+      memcpy(self.kf.decor_obs_cov, &decor_obs_cov[0], self.obs_dim * sizeof(double))
