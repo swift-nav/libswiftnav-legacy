709fb48dc427446e48363a6326b6c50fde439b21
Ian Horn <horn.imh@gmail.com>
2014-02-17 16:42:28 -0800

    using kf struct throughout

--- swiftnav/float_kf.pyx
+++ swiftnav/float_kf.pyx
@@ -63,8 +63,6 @@ def update_scalar_measurement(h, R, U, D):
     np.array(D, dtype=np.double)
   cdef np.ndarray[np.double_t, ndim=1, mode="c"] k = \
     np.empty(state_dim, dtype=np.double)
-  # cdef np.ndarray[np.double_t, ndim=1, mode="c"] R_ = \
-  #   np.array([R], dtype=np.double)
 
   float_kf_c.update_scalar_measurement(state_dim,
                                        <double *> &h_[0],
@@ -74,33 +72,44 @@ def update_scalar_measurement(h, R, U, D):
                                        <double *> &k[0])
   return UDU_decomposition(U_, D_), k
 
-def update_for_obs(decor_obs_mtx, decor_obs_cov,
-                   intermediate_mean, intermediate_cov_U, intermediate_cov_D,
+def update_for_obs(KalmanFilter kf,
+                   intermediate_mean, intermediate_cov_UDU,
                    decor_obs):
-  state_dim = intermediate_cov_D.shape[0]
-  obs_dim = decor_obs.shape[0]
-
-  cdef np.ndarray[np.double_t, ndim=2, mode="c"] decor_obs_mtx_ = \
-    np.array(decor_obs_mtx, dtype=np.double)
-  cdef np.ndarray[np.double_t, ndim=1, mode="c"] decor_obs_cov_ = \
-    np.array(decor_obs_cov, dtype=np.double)
   cdef np.ndarray[np.double_t, ndim=1, mode="c"] intermediate_mean_ = \
     np.array(intermediate_mean, dtype=np.double)
   cdef np.ndarray[np.double_t, ndim=2, mode="c"] intermediate_cov_U_ = \
-    np.array(intermediate_cov_U, dtype=np.double)
+    np.array(intermediate_cov_UDU.U, dtype=np.double)
   cdef np.ndarray[np.double_t, ndim=1, mode="c"] intermediate_cov_D_ = \
-    np.array(intermediate_cov_D, dtype=np.double)
+    np.array(intermediate_cov_UDU.D, dtype=np.double)
   cdef np.ndarray[np.double_t, ndim=1, mode="c"] decor_obs_ =  \
     np.array(decor_obs, dtype=np.double)
 
-  float_kf_c.update_for_obs(state_dim, obs_dim,
-                            <double *> &decor_obs_mtx_[0,0],
-                            <double *> &decor_obs_cov_[0], 
+  float_kf_c.update_for_obs(&(kf.kf),
                             <double *> &intermediate_mean_[0], 
                             <double *> &intermediate_cov_U_[0,0], 
                             <double *> &intermediate_cov_D_[0],
                             <double *> &decor_obs_[0])
   return intermediate_mean_, UDU_decomposition(intermediate_cov_U_, intermediate_cov_D_)
+
+def filter_update(KalmanFilter kf,
+                     state_mean, state_cov_UDU,
+                     obs):
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] state_mean_ = \
+    np.array(state_mean, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] state_cov_U_ = \
+    np.array(state_cov_UDU.U, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] state_cov_D_ = \
+    np.array(state_cov_UDU.D, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="c"] obs_ =  \
+    np.array(obs, dtype=np.double)
+
+  float_kf_c.filter_update(&(kf.kf),
+                           <double *> &state_mean_[0], 
+                           <double *> &state_cov_U_[0,0],
+                           <double *> &state_cov_D_[0], 
+                           <double *> &obs_[0])
+
+  return state_mean_, UDU_decomposition(state_cov_U_, state_cov_D_)
 
 class UDU_decomposition:
   def __init__(self, U, D):
