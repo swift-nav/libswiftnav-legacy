7192e462b2d16c5b534dbcae345844e1a49abb95
Fergus Noble <fergusnoble@gmail.com>
2014-02-22 19:17:06 -0800

    Fix bug with RTCMv3 PRN decode Added unit test for PRNs

--- src/rtcm3.c
+++ src/rtcm3.c
@@ -353,7 +353,7 @@ s8 rtcm3_decode_1002(u8 *buff, u16 *id, double *tow, u8 *n_sat,
   u16 bit = 64;
   for (u8 i=0; i<*n_sat; i++) {
     /* TODO: Handle SBAS prns properly, numbered differently in RTCM? */
-    nm[i].prn = getbitu(buff, bit, 6); bit += 6;
+    nm[i].prn = getbitu(buff, bit, 6) - 1; bit += 6;
 
     u8 code = getbitu(buff, bit, 1); bit += 1;
     /* TODO: When we start storing the signal/system etc. properly we can
