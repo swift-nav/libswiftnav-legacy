bc63d4d2b535cb5cda3235aa4c5035f45c94cd36
Fergus Noble <fergusnoble@gmail.com>
2014-02-27 13:11:39 -0800

    Adding artih.h for Cortex M4 cross-compile Generated by running artihchk.c on Piksi. See piksi_firmware/tests/arithchk.

---
+++ clapack-3.2.1-CMAKE/F2CLIBS/libf2c/arith_cortex-m4.h
@@ -0,0 +1,7 @@
+/* arith.h definitions for ARM Cortex-M4
+ * Calculated by running arithchk on an STM32F4 target. */
+#define IEEE_8087
+#define Arith_Kind_ASL 1
+#define Double_Align
+#define QNaN0 0x0
+#define QNaN1 0x7ff80000
