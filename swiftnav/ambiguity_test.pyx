7af2862c11dc1fcf2c59d674e20270a148baba93
Ian Horn <horn.imh@gmail.com>
2014-03-06 17:05:45 -0800

    added C phase_obs_null_basis

--- swiftnav/ambiguity_test.pyx
+++ swiftnav/ambiguity_test.pyx
@@ -8,6 +8,16 @@
 # WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 
 cimport ambiguity_test_c
+import numpy as np
+cimport numpy as np
 
-def bork(foo):
-  print foo
+def get_phase_obs_null_basis(DE):
+  num_dds = DE.shape[0]
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] DE_ = \
+    np.array(DE, dtype=np.double)
+
+  cdef np.ndarray[np.double_t, ndim=2, mode="c"] q_ = \
+    np.empty((num_dds-3,num_dds), dtype=np.double)
+
+  ambiguity_test_c.assign_phase_obs_null_basis(DE.shape[0]+1, &DE_[0,0], &q_[0,0])
+  return q_
