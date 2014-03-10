e9515e7687e97ca4f00da6d8d6bce740722b3821
Ian Horn <horn.imh@gmail.com>
2014-03-10 12:22:19 -0700

    added full residual computations

--- include/libswiftnav/ambiguity_test.h
+++ include/libswiftnav/ambiguity_test.h
@@ -19,14 +19,14 @@ typedef struct {
   u32 res_dim;
   u8 null_space_dim;
   double null_projector[(MAX_CHANNELS-4) * (MAX_CHANNELS-1)];
-  double res_cov_inv[(2*MAX_CHANNELS - 5) * (2*MAX_CHANNELS - 5)];
+  double half_res_cov_inv[(2*MAX_CHANNELS - 5) * (2*MAX_CHANNELS - 5)];
 } residual_mtxs_t;
 
 void init_residual_matrices(residual_mtxs_t *res_mtxs, u8 num_dds, double *DE_mtx, double *obs_cov);
 void assign_phase_obs_null_basis(u8 num_dds, double *DE_mtx, double *q);
 void assign_residual_covariance_inverse(u8 num_dds, double *obs_cov, double *q, double *r_cov_inv);
 void assign_r_vec(residual_mtxs_t *res_mtxs, u8 num_dds, double *dd_measurements, double *r_vec);
-void assign_r_mean(residual_mtxs_t *res_mtxs, u8 num_dds, s8 *hypothesis, double *r_mean);
-double get_quadratic_term(residual_mtxs_t *res_mtxs, u8 num_dds, s8 *hypothesis, double *r_vec);
+void assign_r_mean(residual_mtxs_t *res_mtxs, u8 num_dds, double *hypothesis, double *r_mean);
+double get_quadratic_term(residual_mtxs_t *res_mtxs, u8 num_dds, double *hypothesis, double *r_vec);
 
 #endif /* LIBSWIFTNAV_AMBIGUITY_TEST_H */
--- src/ambiguity_test.c
+++ src/ambiguity_test.c
@@ -21,8 +21,10 @@
 
 void init_residual_matrices(residual_mtxs_t *res_mtxs, u8 num_dds, double *DE_mtx, double *obs_cov)
 {
+  res_mtxs->res_dim = 2 * num_dds - 3;
+  res_mtxs->null_space_dim = num_dds - 3;
   assign_phase_obs_null_basis(num_dds, DE_mtx, res_mtxs->null_projector);
-  assign_residual_covariance_inverse(num_dds, obs_cov, res_mtxs->null_projector, res_mtxs->res_cov_inv);
+  assign_residual_covariance_inverse(num_dds, obs_cov, res_mtxs->null_projector, res_mtxs->half_res_cov_inv);
 }
 
 
@@ -121,7 +123,7 @@ void assign_residual_covariance_inverse(u8 num_dds, double *obs_cov, double *q, 
   //TODO make more efficient via the structure of q_tilde, and it's relation to the I + 1*1^T structure of the obs cov mtx
   cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans, // CBLAS_ORDER, CBLAS_TRANSPOSE transA, cBLAS_TRANSPOSE transB
               res_dim, res_dim, dd_dim, // int M, int N, int K
-              1, QC, dd_dim, // double alpha, double *A, int lda
+              2, QC, dd_dim, // double alpha, double *A, int lda
               q_tilde, dd_dim, //double *B, int ldb
               0, r_cov_inv, res_dim); //beta, double *C, int ldc
   // MAT_PRINTF(r_cov_inv, res_dim, res_dim);
@@ -133,24 +135,53 @@ void assign_residual_covariance_inverse(u8 num_dds, double *obs_cov, double *q, 
   char uplo = 'L'; //actually this makes it upper. the lapack stuff is all column major, but we're good since we're operiating on symmetric matrices
   integer info;
   dpotrf_(&uplo, &res_dim, r_cov_inv, &res_dim, &info);
-  printf("info: %i\n", (int) info);
+  // printf("info: %i\n", (int) info);
   // MAT_PRINTF(r_cov_inv, res_dim, res_dim);
   dpotri_(&uplo, &res_dim, r_cov_inv, &res_dim, &info);
-  printf("info: %i\n", (int) info);
+  // printf("info: %i\n", (int) info);
   // MAT_PRINTF(r_cov_inv, res_dim, res_dim);
 }
 
-// void assign_r_vec(residual_mtxs_t *res_mtxs, u8 num_dds, s8 *dd_measurements, double *r_vec)
-// {
+void assign_r_vec(residual_mtxs_t *res_mtxs, u8 num_dds, double *dd_measurements, double *r_vec)
+{
+  cblas_dgemv(CblasRowMajor, CblasNoTrans,
+              res_mtxs->null_space_dim, num_dds,
+              1, res_mtxs->null_projector, num_dds,
+              dd_measurements, 1,
+              0, r_vec, 1);
+  for (u8 i=0; i< num_dds; i++) {
+    r_vec[i + res_mtxs->null_space_dim] = dd_measurements[i] - dd_measurements[i+num_dds] / GPS_L1_LAMBDA_NO_VAC;
+  }
+}
 
-// }
+void assign_r_mean(residual_mtxs_t *res_mtxs, u8 num_dds, double *hypothesis, double *r_mean)
+{
+  cblas_dgemv(CblasRowMajor, CblasNoTrans,
+              res_mtxs->null_space_dim, num_dds,
+              1, res_mtxs->null_projector, num_dds,
+              hypothesis, 1,
+              0, r_mean, 1);
+  memcpy(&r_mean[res_mtxs->null_space_dim], hypothesis, num_dds * sizeof(double));
+}
 
-// void assign_r_mean(residual_mtxs_t *res_mtxs, u8 num_dds, s8 *hypothesis, double *r_mean)
-// {
+double get_quadratic_term(residual_mtxs_t *res_mtxs, u8 num_dds, double *hypothesis, double *r_vec)
+{
+  double r[res_mtxs->res_dim];
+  assign_r_mean(res_mtxs, num_dds, hypothesis, r);
+  for (u32 i=0; i<res_mtxs->res_dim; i++) {
+    r[i] = r_vec[i] - r[i];
+  }
+  double half_sig_dot_r[res_mtxs->res_dim];
+  cblas_dsymv(CblasRowMajor, CblasUpper,
+                 res_mtxs->res_dim,
+                 1, res_mtxs->half_res_cov_inv, res_mtxs->res_dim,
+                 r, 1,
+                 0, half_sig_dot_r, 1);
+  double quad_term = 0;
+  for (u32 i=0; i<res_mtxs->res_dim; i++) {
+    quad_term -= half_sig_dot_r[i] * r[i];
+  }
+  return quad_term;
+}
 
-// }
 
-// double get_quadratic_term(residual_mtxs_t *res_mtxs, u8 num_dds, s8 *hypothesis, double *r_vec)
-// {
-
-// }
