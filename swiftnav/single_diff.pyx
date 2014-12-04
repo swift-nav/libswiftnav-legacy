c6bdc4c69a064677ef1249b9001b0c77a8d455b9
Ian Horn <horn.imh@gmail.com>
2014-12-04 09:35:03 -0800

    Filling out whole sdiff struct

--- swiftnav/single_diff.pyx
+++ swiftnav/single_diff.pyx
@@ -15,8 +15,12 @@ from libc.string cimport memcpy
 cdef class SingleDiff:
   cdef single_diff_c.sdiff_t sdiff
   def __init__(self,
+               pseudorange,
+               carrier_phase,
+               doppler,
                np.ndarray[np.double_t, ndim=1, mode="c"] sat_pos, 
                np.ndarray[np.double_t, ndim=1, mode="c"] sat_vel,
+               snr,
                prn):
     self.sat_pos = sat_pos
     self.sat_vel = sat_vel
@@ -24,6 +28,24 @@ cdef class SingleDiff:
 
   def __repr__(self):
     return "<SingleDiff prn=" + str(self.prn) + ">"
+
+  property pseudorange:
+    def __get__(self):
+        return self.sdiff.pseudorange
+    def __set__(self, pseudorange):
+        self.sdiff.pseudorange = pseudorange
+  
+  property carrier_phase:
+    def __get__(self):
+        return self.sdiff.carrier_phase
+    def __set__(self, carrier_phase):
+        self.sdiff.carrier_phase = carrier_phase
+
+  property doppler:
+    def __get__(self):
+        return self.sdiff.doppler
+    def __set__(self, doppler):
+        self.sdiff.doppler = doppler
 
   property sat_pos:
     def __get__(self):
@@ -42,6 +64,12 @@ cdef class SingleDiff:
       return sat_vel
     def __set__(self, np.ndarray[np.double_t, ndim=1, mode="c"] sat_vel):
       memcpy(self.sdiff.sat_vel, &sat_vel[0], 3 * sizeof(double))
+
+  property snr:
+    def __get__(self):
+        return self.sdiff.snr
+    def __set__(self, snr):
+        self.sdiff.snr = snr
 
   property prn:
     def __get__(self):
