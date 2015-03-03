2e357e0f905e296af9a6fe8d2789cba4e7860ae3
Ian Horn <horn.imh@gmail.com>
2015-03-02 17:48:03 -0800

    Made copy_sdiffs_put_ref_first take const arguments

--- include/libswiftnav/single_diff.h
+++ include/libswiftnav/single_diff.h
@@ -49,7 +49,7 @@ u8 make_propagated_sdiffs(u8 n_local, navigation_measurement_t *m_local,
 
 void almanacs_to_single_diffs(u8 n, almanac_t *alms, gps_time_t timestamp, sdiff_t *sdiffs);
 
-s8 copy_sdiffs_put_ref_first(u8 ref_prn, u8 num_sdiffs, sdiff_t *sdiffs, sdiff_t *sdiffs_with_ref_first);
+s8 copy_sdiffs_put_ref_first(const u8 ref_prn, const u8 num_sdiffs, const sdiff_t *sdiffs, sdiff_t *sdiffs_with_ref_first);
 
 u8 filter_sdiffs(u8 num_sdiffs, sdiff_t *sdiffs, u8 num_sats_to_drop, u8 *sats_to_drop);
 
