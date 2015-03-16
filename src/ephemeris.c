6cf7ba1b3053fe22f0fbf8b26304aeb1476200e6
Fergus Noble <fergusnoble@gmail.com>
2015-03-15 22:52:52 -0700

    NAV MSG: Move ephemeris decoding into ephemeris.c

--- src/ephemeris.c
+++ src/ephemeris.c
@@ -184,6 +184,158 @@ u8 ephemeris_good(ephemeris_t *eph, gps_time_t t)
 
   /* TODO: this doesn't exclude ephemerides older than a week so could be made
    * better. */
-  return (eph->valid  && eph->healthy && fabs(dt) < 4*3600);
+  return (eph->valid && eph->healthy && fabs(dt) < 4*3600);
+}
+
+/** Decode ephemeris from L1 C/A GPS navigation message frames.
+ *
+ * \note This function does not check for parity errors. You should check the
+ *       subframes for parity errors before calling this function.
+ *
+ * References:
+ *   -# IS-GPS-200D, Section 20.3.2 and Figure 20-1
+ *
+ * \param frame_words Array containing words 3 through 10 of subframes
+ *                    1, 2 and 3. Word is in the 30 LSBs of the u32.
+ * \param e Pointer to an ephemeris struct to fill in.
+ */
+void decode_ephemeris(u32 frame_words[3][8], ephemeris_t *e)
+{
+  assert(frame_words != NULL);
+  assert(e != NULL);
+
+  /* These unions facilitate signed/unsigned conversion and sign extension. */
+  union {
+    s8 s8;
+    u8 u8;
+  } onebyte;
+
+  union
+  {
+    s16 s16;
+    u16 u16;
+  } twobyte;
+
+  union
+  {
+    s32 s32;
+    u32 u32;
+  } fourbyte;
+
+  /* Subframe 1: SV health, T_GD, t_oc, a_f2, a_f1, a_f0 */
+
+  /* GPS week number (mod 1024): Word 3, bits 20-30 */
+  e->toe.wn = (frame_words[0][3-3] >> (30-10) & 0x3FF);
+  e->toe.wn += GPS_WEEK_CYCLE * 1024;
+  e->toc.wn = e->toe.wn;
+
+  /* Health flag: Word 3, bit 17 */
+  e->healthy = !(frame_words[0][3-3] >> (30-17) & 1);
+
+  /* t_gd: Word 7, bits 17-24 */
+  onebyte.u8 = frame_words[0][7-3] >> (30-24) & 0xFF;
+  e->tgd = onebyte.s8 * pow(2,-31);
+
+  /* t_oc: Word 8, bits 8-24 */
+  e->toc.tow = (frame_words[0][8-3] >> (30-24) & 0xFFFF) * 16;
+
+  /* a_f2: Word 9, bits 1-8 */
+  onebyte.u8 = frame_words[0][9-3] >> (30-8) & 0xFF;
+  e->af2 = onebyte.s8 * pow(2,-55);
+
+  /* a_f1: Word 9, bits 9-24 */
+  twobyte.u16 = frame_words[0][9-3] >> (30-24) & 0xFFFF;
+  e->af1 = twobyte.s16 * pow(2,-43);
+
+  /* a_f0: Word 10, bits 1-22 */
+  fourbyte.u32 = frame_words[0][10-3] >> (30-22) & 0x3FFFFF;
+  /* Shift to the left for sign extension */
+  fourbyte.u32 <<= 10;
+  /* Carry the sign bit back down and reduce to signed 22 bit value */
+  fourbyte.s32 >>= 10;
+  e->af0 = fourbyte.s32 * pow(2,-31);
+
+  /* Subframe 2: crs, dn, m0, cuc, ecc, cus, sqrta, toe */
+
+  /* crs: Word 3, bits 9-24 */
+  twobyte.u16 = frame_words[1][3-3] >> (30-24) & 0xFFFF;
+  e->crs = twobyte.s16 * pow(2,-5);
+
+  /* dn: Word 4, bits 1-16 */
+  twobyte.u16 = frame_words[1][4-3] >> (30-16) & 0xFFFF;
+  e->dn = twobyte.s16 * pow(2,-43) * GPS_PI;
+
+  /* m0: Word 4, bits 17-24 and word 5, bits 1-24 */
+  fourbyte.u32 = ((frame_words[1][4-3] >> (30-24) & 0xFF) << 24)
+               | (frame_words[1][5-3] >> (30-24) & 0xFFFFFF);
+  e->m0 = fourbyte.s32 * pow(2,-31) * GPS_PI;
+
+  /* cuc: Word 6, bits 1-16 */
+  twobyte.u16 = frame_words[1][6-3] >> (30-16) & 0xFFFF;
+  e->cuc = twobyte.s16 * pow(2,-29);
+
+  /* ecc: Word 6, bits 17-24 and word 7, bits 1-24 */
+  fourbyte.u32 = ((frame_words[1][6-3] >> (30-24) & 0xFF) << 24)
+               | (frame_words[1][7-3] >> (30-24) & 0xFFFFFF);
+  e->ecc = fourbyte.u32 * pow(2,-33);
+
+  /* cus: Word 8, bits 1-16 */
+  twobyte.u16 = frame_words[1][8-3] >> (30-16) & 0xFFFF;
+  e->cus = twobyte.s16 * pow(2,-29);
+
+  /* sqrta: Word 8, bits 17-24 and word 9, bits 1-24 */
+  fourbyte.u32 = ((frame_words[1][8-3] >> (30-24) & 0xFF) << 24)
+               | (frame_words[1][9-3] >> (30-24) & 0xFFFFFF);
+  e->sqrta = fourbyte.u32 * pow(2,-19);
+
+  /* t_oe: Word 10, bits 1-16 */
+  e->toe.tow = (frame_words[1][10-3] >> (30-16) & 0xFFFF) * 16;
+
+
+  /* Subframe 3: cic, omega0, cis, inc, crc, w, omegadot, inc_dot */
+
+  /* cic: Word 3, bits 1-16 */
+  twobyte.u16 = frame_words[2][3-3] >> (30-16) & 0xFFFF;
+  e->cic = twobyte.s16 * pow(2,-29);
+
+  /* omega0: Word 3, bits 17-24 and word 4, bits 1-24 */
+  fourbyte.u32 = ((frame_words[2][3-3] >> (30-24) & 0xFF) << 24)
+               | (frame_words[2][4-3] >> (30-24) & 0xFFFFFF);
+  e->omega0 = fourbyte.s32 * pow(2,-31) * GPS_PI;
+
+  /* cis: Word 5, bits 1-16 */
+  twobyte.u16 = frame_words[2][5-3] >> (30-16) & 0xFFFF;
+  e->cis = twobyte.s16 * pow(2,-29);
+
+  /* inc (i0): Word 5, bits 17-24 and word 6, bits 1-24 */
+  fourbyte.u32 = ((frame_words[2][5-3] >> (30-24) & 0xFF) << 24)
+               | (frame_words[2][6-3] >> (30-24) & 0xFFFFFF);
+  e->inc = fourbyte.s32 * pow(2,-31) * GPS_PI;
+
+  /* crc: Word 7, bits 1-16 */
+  twobyte.u16 = frame_words[2][7-3] >> (30-16) & 0xFFFF;
+  e->crc = twobyte.s16 * pow(2,-5);
+
+  /* w (omega): Word 7, bits 17-24 and word 8, bits 1-24 */
+  fourbyte.u32 = ((frame_words[2][7-3] >> (30-24) & 0xFF) << 24)
+               | (frame_words[2][8-3] >> (30-24) & 0xFFFFFF);
+  e->w = fourbyte.s32 * pow(2,-31) * GPS_PI;
+
+  /* Omega_dot: Word 9, bits 1-24 */
+  fourbyte.u32 = frame_words[2][9-3] >> (30-24) & 0xFFFFFF;
+  /* Shift left for sign extension */
+  fourbyte.u32 <<= 8;
+  /* Sign extend it */
+  fourbyte.s32 >>= 8;
+  e->omegadot = fourbyte.s32 * pow(2,-43) * GPS_PI;
+
+  /* inc_dot (IDOT): Word 10, bits 9-22 */
+  twobyte.u16 = frame_words[2][10-3] >> (30-22) & 0x3FFF;
+  twobyte.u16 <<= 2;
+  /* Sign extend */
+  twobyte.s16 >>= 2;
+  e->inc_dot = twobyte.s16 * pow(2,-43) * GPS_PI;
+
+  e->valid = 1;
 }
 
