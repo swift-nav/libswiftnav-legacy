ee90059263d9088c9394a65ac490f4c3081510e2
Fergus Noble <fergusnoble@gmail.com>
2015-03-22 14:46:50 -0700

    BASELINE: Move least squares solver out of amb_kf
    HACK WARNING: clapack.h and lapacke.h are interacting, until we dispense with one of them we need to hack around it.

--- include/libswiftnav/baseline.h
+++ include/libswiftnav/baseline.h
@@ -14,11 +14,15 @@
 #define LIBSWIFTNAV_BASELINE_H
 
 #include "common.h"
+#include "single_diff.h"
 
 void amb_from_baseline(u8 num_sats, double *DE, double *dd_meas,
                        double b[3], s32 *N);
 void lesq_solution(u8 num_dds, double *dd_meas, s32 *N, double *DE,
                    double b[3], double *resid);
+void least_squares_solve_b_external_ambs(u8 num_dds, const double *ambs,
+         const sdiff_t *sdiffs_with_ref_first, const double *dd_measurements,
+         const double ref_ecef[3], double b[3]);
 
 #endif /* LIBSWIFTNAV_BASELINE_H */
 
