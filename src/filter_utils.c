c9c5360aa175ee6dd0ec6ec675b9fd9d2ac07256
Fergus Noble <fergusnoble@gmail.com>
2015-04-21 00:38:36 -0700

    FILTER UTILS: Simplify assign_de_matrix
    Still passes tests!

--- src/filter_utils.c
+++ src/filter_utils.c
@@ -26,52 +26,27 @@
 s8 assign_de_mtx(u8 num_sats, const sdiff_t *sats_with_ref_first,
                  const double ref_ecef[3], double *DE)
 {
-  DEBUG_ENTRY();
-
-  if (DEBUG) {
-    printf("num_sats = %u\nsdiff prns&positions = {\n", num_sats);
-    for (u8 i=0; i < num_sats; i++) {
-      printf("i = %u, prn = %u, \tpos = [%f, \t%f, \t%f]\n",
-             i,
-             sats_with_ref_first[i].prn,
-             sats_with_ref_first[i].sat_pos[0],
-             sats_with_ref_first[i].sat_pos[1],
-             sats_with_ref_first[i].sat_pos[2]);
-    }
-    printf("}\nref_ecef = {%f, \t%f, \t%f}\n", ref_ecef[0], ref_ecef[1], ref_ecef[2]);
-  }
-
-  u8 de_length = num_sats - 1;
-
   if (num_sats <= 1) {
-    log_debug("not enough sats\n");
-    DEBUG_EXIT();
+    log_debug("assign_de_mtx: not enough sats\n");
     return -1;
   }
+
   assert(num_sats > 1);
 
-  memset(DE, 0, de_length * 3 * sizeof(double));
-  double e0[3];
-  double x0 = sats_with_ref_first[0].sat_pos[0] - ref_ecef[0];
-  double y0 = sats_with_ref_first[0].sat_pos[1] - ref_ecef[1];
-  double z0 = sats_with_ref_first[0].sat_pos[2] - ref_ecef[2];
-  double norm0 = sqrt(x0*x0 + y0*y0 + z0*z0);
-  e0[0] = x0 / norm0;
-  e0[1] = y0 / norm0;
-  e0[2] = z0 / norm0;
+  /* Vector to reference satellite. */
+  double e_0[3];
+  vector_subtract(3, sats_with_ref_first[0].sat_pos, ref_ecef, e_0);
+  vector_normalize(3, e_0);
+
   for (u8 i=1; i<num_sats; i++) {
-    double x = sats_with_ref_first[i].sat_pos[0] - ref_ecef[0];
-    double y = sats_with_ref_first[i].sat_pos[1] - ref_ecef[1];
-    double z = sats_with_ref_first[i].sat_pos[2] - ref_ecef[2];
-    double norm = sqrt(x*x + y*y + z*z);
-    DE[3*(i-1)] = x / norm - e0[0];
-    DE[3*(i-1) + 1] = y / norm - e0[1];
-    DE[3*(i-1) + 2] = z / norm - e0[2];
+    /* Vector to satellite i */
+    double e_i[3];
+    vector_subtract(3, sats_with_ref_first[i].sat_pos, ref_ecef, e_i);
+    vector_normalize(3, e_i);
+    /* DE row = e_i - e_0 */
+    vector_subtract(3, e_i, e_0, &DE[3*(i-1)]);
   }
-  if (DEBUG) {
-    MAT_PRINTF(DE, (num_sats-1), 3);
-  }
-  DEBUG_EXIT();
+
   return 0;
 }
 
