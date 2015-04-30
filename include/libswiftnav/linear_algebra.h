kovach/libswiftnav

updated sat inclusion process to use s64's to avoid overflow issue

include/libswiftnav/linear_algebra.h
@@ -50,6 +50,8 @@ void matrix_multiply(u32 n, u32 m, u32 p, const double *a,
                      const double *b, double *c);
 void matrix_multiply_i(u32 n, u32 m, u32 p, const s32 *a,
                        const s32 *b, s32 *c);
+void matrix_multiply_i64(u32 n, u32 m, u32 p, const s64 *a,
+                         const s64 *b, s64 *c);
 void matrix_triu(u32 n, double *M);
 void matrix_eye(u32 n, double *M);
 void matrix_udu(u32 n, double *M, double *U, double *D);

src/linear_algebra.c
@@ -630,6 +630,18 @@ inline void matrix_multiply_i(u32 n, u32 m, u32 p, const s32 *a,
     }
 }
 
+inline void matrix_multiply_i64(u32 n, u32 m, u32 p, const s64 *a,
+                                const s64 *b, s64 *c)
+{
+  u32 i, j, k;
+  for (i = 0; i < n; i++)
+    for (j = 0; j < p; j++) {
+      c[p*i + j] = 0;
+      for (k = 0; k < m; k++)
+        c[p*i + j] += a[m*i+k] * b[p*k + j];
+    }
+}
+
 /** Zero lower triangle of an `n` x `n` square matrix.
  * Some routines designed to work on upper triangular matricies use the lower
  * triangle as scratch space. This function zeros the lower triangle such that

swift-nav/libswiftnav-legacy

include/libswiftnav/linear_algebra.h
@@ -50,6 +50,8 @@ void matrix_multiply(u32 n, u32 m, u32 p, const double *a,
                      const double *b, double *c);
 void matrix_multiply_i(u32 n, u32 m, u32 p, const s32 *a,
                        const s32 *b, s32 *c);
+void matrix_multiply_s64(u32 n, u32 m, u32 p, const s64 *a,
+                         const s64 *b, s64 *c);
 void matrix_triu(u32 n, double *M);
 void matrix_eye(u32 n, double *M);
 void matrix_udu(u32 n, double *M, double *U, double *D);

src/linear_algebra.c
@@ -630,6 +630,18 @@ inline void matrix_multiply_i(u32 n, u32 m, u32 p, const s32 *a,
     }
 }
 
+inline void matrix_multiply_s64(u32 n, u32 m, u32 p, const s64 *a,
+                                const s64 *b, s64 *c)
+{
+  u32 i, j, k;
+  for (i = 0; i < n; i++)
+    for (j = 0; j < p; j++) {
+      c[p*i + j] = 0;
+      for (k = 0; k < m; k++)
+        c[p*i + j] += a[m*i+k] * b[p*k + j];
+    }
+}
+
 /** Zero lower triangle of an `n` x `n` square matrix.
  * Some routines designed to work on upper triangular matricies use the lower
  * triangle as scratch space. This function zeros the lower triangle such that