7bb15523190293d60d177e047fc35eceec3d99ff
Fergus Noble <fergusnoble@gmail.com>
2015-06-05 11:22:21 -0700

    EPHEMERIS: Add unit test for equality function

--- tests/check_main.c
+++ tests/check_main.c
@@ -22,6 +22,7 @@ int main(void)
   srunner_add_suite(sr, coord_system_suite());
   srunner_add_suite(sr, linear_algebra_suite());
   srunner_add_suite(sr, filter_utils_suite());
+  srunner_add_suite(sr, ephemeris_suite());
 
   srunner_set_fork_status(sr, CK_NOFORK);
   srunner_run_all(sr, CK_NORMAL);
