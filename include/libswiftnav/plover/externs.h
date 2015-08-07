02e1afa18b6580c2418124c9f8ecc34be4677242
Scott Kovach <dskovach@gmail.com>
2015-08-06 18:00:02 -0700

    include h/c files

---
+++ include/libswiftnav/plover/externs.h
@@ -0,0 +1,10 @@
+#ifndef PLOVER_GENERATED_externs
+#define PLOVER_GENERATED_externs
+
+
+#include "common.h"
+void newline (void);
+void print_matrix (const s32 n, const s32 m, const double * A);
+
+
+#endif /* PLOVER_GENERATED_externs */
