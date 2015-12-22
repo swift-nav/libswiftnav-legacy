75d64f2d1fb0e790d2ead02d63d02f97940f3beb
Leith Bade <leith@swiftnav.com>
2015-12-22 13:10:22 +1100

    Add testcase for gps_time_match_weeks

--- src/time.c
+++ src/time.c
@@ -86,8 +86,8 @@ double gpsdifftime(gps_time_t end, gps_time_t beginning)
   return dt;
 }
 
-/** Set the week number of t so as to minimize the magnitude of the
- * time difference between t and ref.
+/** Given an uknown week number in t, fill in the week number from ref
+ * under the assumption the two times are seperated by less than a week.
  *
  * \param t Pointer to GPS time whose week number will be set
  * \param ref Reference GPS time
--- tests/check_time.c
+++ tests/check_time.c
@@ -52,6 +52,35 @@ START_TEST(test_normalize_gps_time)
 }
 END_TEST
 
+START_TEST(test_gps_time_match_weeks)
+{
+  struct gps_time_match_weeks_testcase {
+    gps_time_t t, ref, ret;
+  } testcases[] = {
+    {.t = {0.0, WN_UNKNOWN}, .ref = {0.0, 1234}, .ret = {0.0, 1234}},
+    {.t = {WEEK_SECS-1, WN_UNKNOWN}, .ref = {0.0, 1234}, .ret = {WEEK_SECS-1, 1233}},
+    {.t = {0.0, WN_UNKNOWN}, .ref = {WEEK_SECS-1, 1234}, .ret = {0.0, 1235}},
+    {.t = {WEEK_SECS-1, WN_UNKNOWN}, .ref = {WEEK_SECS-1, 1234}, .ret = {WEEK_SECS-1, 1234}},
+    {.t = {2*DAY_SECS, WN_UNKNOWN}, .ref = {5*DAY_SECS, 1234}, .ret = {2*DAY_SECS, 1234}},
+    {.t = {5*DAY_SECS, WN_UNKNOWN}, .ref = {2*DAY_SECS, 1234}, .ret = {5*DAY_SECS, 1234}},
+    {.t = {0.0, WN_UNKNOWN}, .ref = {WEEK_SECS/2, 1234}, .ret = {0.0, 1234}},
+    {.t = {WEEK_SECS/2, WN_UNKNOWN}, .ref = {0.0, 1234}, .ret = {WEEK_SECS/2, 1234}},
+    {.t = {WEEK_SECS/2+1, WN_UNKNOWN}, .ref = {0.0, 1234}, .ret = {WEEK_SECS/2+1, 1233}},
+    {.t = {0.0, WN_UNKNOWN}, .ref = {WEEK_SECS/2+1, 1234}, .ret = {0.0, 1235}},
+  };
+  for (size_t i = 0;
+       i < sizeof(testcases) / sizeof(struct gps_time_match_weeks_testcase); i++) {
+    gps_time_match_weeks(&testcases[i].t, &testcases[i].ref);
+    fail_unless(testcases[i].t.wn == testcases[i].ret.wn,
+                "gps_time_match_weeks test case %d failed, t.wn = %d, ret.wn = %d",
+                i, testcases[i].t.wn, testcases[i].ret.wn);
+    fail_unless(testcases[i].t.tow == testcases[i].ret.tow,
+                "gps_time_match_weeks test case %d failed, t.tow = %.12f, ret.tow = %.12f",
+                i, testcases[i].t.tow, testcases[i].ret.tow);
+  }
+}
+END_TEST
+
 Suite* time_test_suite(void)
 {
   Suite *s = suite_create("Time handling");
@@ -59,6 +88,7 @@ Suite* time_test_suite(void)
   TCase *tc_core = tcase_create("Core");
   tcase_add_test(tc_core, test_gpsdifftime);
   tcase_add_test(tc_core, test_normalize_gps_time);
+  tcase_add_test(tc_core, test_gps_time_match_weeks);
   suite_add_tcase(s, tc_core);
 
   return s;
