29feae67f73710c11ec3a9ea6718a3b13903233a
Jacob McNamee <jacob@swiftnav.com>
2016-02-14 18:53:03 -0800

    Update signal docs

--- include/libswiftnav/signal.h
+++ include/libswiftnav/signal.h
@@ -16,17 +16,24 @@
 
 #include <libswiftnav/common.h>
 
+/** \addtogroup signal
+ * \{ */
+
+/* Number of satellites in each constellation. */
 #define NUM_SATS_GPS 32
 #define NUM_SATS_SBAS 19
 #define NUM_SATS (NUM_SATS_GPS + NUM_SATS_SBAS)
 
+/* Number of codes in each constellation. */
 #define NUM_CODES_GPS 2
 #define NUM_CODES_SBAS 1
 
+/* Number of signals in each code. */
 #define NUM_SIGNALS_GPS_L1CA NUM_SATS_GPS
 #define NUM_SIGNALS_GPS_L2CM NUM_SATS_GPS
 #define NUM_SIGNALS_SBAS_L1CA NUM_SATS_SBAS
 
+/* Number of signals in each constellation. */
 #define NUM_SIGNALS_GPS (NUM_SIGNALS_GPS_L1CA + NUM_SIGNALS_GPS_L2CM)
 #define NUM_SIGNALS_SBAS (NUM_SIGNALS_SBAS_L1CA)
 #define NUM_SIGNALS (NUM_SIGNALS_GPS + NUM_SIGNALS_SBAS)
@@ -36,6 +43,7 @@
 
 #define SID_STR_LEN_MAX 16
 
+/** Constellation identifier. */
 enum constellation {
   CONSTELLATION_INVALID = -1,
   CONSTELLATION_GPS,
@@ -43,6 +51,7 @@ enum constellation {
   CONSTELLATION_COUNT,
 };
 
+/** Code identifier. */
 enum code {
   CODE_INVALID = -1,
   CODE_GPS_L1CA,
@@ -51,11 +60,13 @@ enum code {
   CODE_COUNT,
 };
 
+/** GNSS signal identifier. */
 typedef struct {
   u16 sat;
   enum code code;
 } gnss_signal_t;
 
+/** Signal comparison function. */
 static inline int sid_compare(const gnss_signal_t a, const gnss_signal_t b)
 {
   s32 x;
@@ -64,45 +75,26 @@ static inline int sid_compare(const gnss_signal_t a, const gnss_signal_t b)
   return a.sat - b.sat;
 }
 
+/** Untyped signal comparison function. */
 static inline int cmp_sid_sid(const void *a, const void *b)
 {
   return sid_compare(*(gnss_signal_t *)a, *(gnss_signal_t*)b);
 }
 
+/** Signal equality function. */
 static inline bool sid_is_equal(const gnss_signal_t a, const gnss_signal_t b)
 {
   return sid_compare(a, b) == 0;
 }
 
-/** Constructs and returns a gnss_signal_t structure using given parameters.
- */
+/* \} */
+
 gnss_signal_t construct_sid(enum code code, u16 sat);
-
-/** Print a string representation of sid to the buffer s of capacity n.
- *  Returns the number of characters printed, excluding the terminating null.
- */
 int sid_to_string(char *s, int n, gnss_signal_t sid);
-
-/** Returns true if sid corresponds to a known constellation, code, and
- *  satellite identifier.
- */
 bool sid_valid(gnss_signal_t sid);
-
-/** Converts a code-specific index in [0, NUM_SIGNALS_<code>) to
- *  the corresponding sid.
- */
 gnss_signal_t sid_from_code_index(enum code code, u16 code_index);
-
-/** Returns the code index in [0, NUM_SIGNALS_<code>) corresponding to the sid.
- */
 u16 sid_to_code_index(gnss_signal_t sid);
-
-/** Returns the constellation which the sid belongs to.
- */
 enum constellation sid_to_constellation(gnss_signal_t sid);
-
-/** Returns the constellation which the code belongs to.
- */
 enum constellation code_to_constellation(enum code code);
 
 #endif /* LIBSWIFTNAV_SIGNAL_H */
--- src/signal.c
+++ src/signal.c
@@ -16,8 +16,13 @@
 
 #include <libswiftnav/signal.h>
 
+/** \defgroup signal GNSS signal identifiers (SID)
+ * \{ */
+
+/** Macro used to assert that the specified code is valid. */
 #define ASSERT_CODE_VALID(code) assert(((code) >= 0) && ((code) < CODE_COUNT))
 
+/** Element in the code data table. */
 typedef struct {
   enum constellation constellation;
   u16 sat_count;
@@ -25,6 +30,7 @@ typedef struct {
   const char *str;
 } code_table_element_t;
 
+/** Table of useful data for each code. */
 static const code_table_element_t code_table[CODE_COUNT] = {
   [CODE_GPS_L1CA] =
     {CONSTELLATION_GPS, NUM_SIGNALS_GPS_L1CA, GPS_FIRST_PRN, "GPS L1CA"},
@@ -34,14 +40,32 @@ static const code_table_element_t code_table[CODE_COUNT] = {
     {CONSTELLATION_SBAS, NUM_SIGNALS_SBAS_L1CA, SBAS_FIRST_PRN, "SBAS L1CA"},
 };
 
+/** String representation used for unknown code values. */
 static const char * unknown_str = "?";
 
+/** Construct a gnss_signal_t.
+ *
+ * \note This function does not check the validity of the resulting signal.
+ *
+ * \param code  Code to use.
+ * \param sat   Satellite identifier to use.
+ *
+ * \return gnss_signal_t corresponding to the specified arguments.
+ */
 gnss_signal_t construct_sid(enum code code, u16 sat)
 {
   gnss_signal_t sid = {.code = code, .sat = sat};
   return sid;
 }
 
+/** Print a string representation of a gnss_signal_t.
+ *
+ * \param s     Buffer of capacity n to which the string will be written.
+ * \param n     Capacity of buffer s.
+ * \param sid   gnss_signal_t to use.
+ *
+ * \return Number of characters written to s, excluding the terminating null.
+ */
 int sid_to_string(char *s, int n, gnss_signal_t sid)
 {
   const char *code_str = ((sid.code < 0) || (sid.code >= CODE_COUNT)) ?
@@ -52,6 +76,13 @@ int sid_to_string(char *s, int n, gnss_signal_t sid)
   return nchars;
 }
 
+/** Determine if a gnss_signal_t corresponds to a known code and
+ * satellite identifier.
+ *
+ * \param sid   gnss_signal_t to use.
+ *
+ * \return true if sid exists, false otherwise.
+ */
 bool sid_valid(gnss_signal_t sid)
 {
   if ((sid.code < 0) || (sid.code >= CODE_COUNT))
@@ -64,6 +95,14 @@ bool sid_valid(gnss_signal_t sid)
   return true;
 }
 
+/** Convert a code-specific signal index to a gnss_signal_t.
+ *
+ * \param code          Code to use.
+ * \param code_index    Code-specific signal index in
+ *                      [0, SIGNAL_COUNT_\<code\>).
+ *
+ * \return gnss_signal_t corresponding to code and code_index.
+ */
 gnss_signal_t sid_from_code_index(enum code code, u16 code_index)
 {
   assert((code >= 0) && (code < CODE_COUNT));
@@ -71,19 +110,39 @@ gnss_signal_t sid_from_code_index(enum code code, u16 code_index)
   return construct_sid(code, code_table[code].sat_start + code_index);
 }
 
+/** Return the code-specific signal index for a gnss_signal_t.
+ *
+ * \param sid   gnss_signal_t to use.
+ *
+ * \return Code-specific signal index in [0, SIGNAL_COUNT_\<code\>).
+ */
 u16 sid_to_code_index(gnss_signal_t sid)
 {
   assert(sid_valid(sid));
   return sid.sat - code_table[sid.code].sat_start;
 }
 
+/** Get the constellation to which a gnss_signal_t belongs.
+ *
+ * \param sid   gnss_signal_t to use.
+ *
+ * \return Constellation to which sid belongs.
+ */
 enum constellation sid_to_constellation(gnss_signal_t sid)
 {
   return code_to_constellation(sid.code);
 }
 
+/** Get the constellation to which a code belongs.
+ *
+ * \param code  Code to use.
+ *
+ * \return Constellation to which code belongs.
+ */
 enum constellation code_to_constellation(enum code code)
 {
   assert((code >= 0) && (code < CODE_COUNT));
   return code_table[code].constellation;
 }
+
+/* \} */
