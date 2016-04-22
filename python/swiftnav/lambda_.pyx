3b0fb354b96c7c1a14cc49df1a434a71cdd85945
Matthew Peddie <mpeddie@gmail.com>
2016-04-23 08:40:57 +1000

    RTKLIB routines use Fortran storage order.

--- python/swiftnav/lambda_.pyx
+++ python/swiftnav/lambda_.pyx
@@ -28,8 +28,8 @@ def lambda_reduction_(Q):
   """
   assert len(Q.shape) == 2 and Q.shape[0] == Q.shape[1], "Q matrix must have shape (n, n)"
   n = Q.shape[0]
-  cdef np.ndarray[np.double_t, ndim=2, mode="c"] Q_ = np.array(Q, dtype=np.double)
-  cdef np.ndarray[np.double_t, ndim=2, mode="c"] Z = np.empty((n, n), dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=2, mode="fortran"] Q_ = np.array(Q, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=2, mode="fortran"] Z = np.empty((n, n), dtype=np.double)
   assert lambda_reduction(n, &Q_[0,0], &Z[0,0]) == 0, "lambda error!"
   return Z
 
@@ -51,9 +51,9 @@ def lambda_solution_(x, sigma, m):
   assert len(sigma.shape) == 2 and sigma.shape[0] == sigma.shape[1], "Q matrix must have shape (n, n)"
   assert len(x.shape) == 1 and x.shape[0] == sigma.shape[1], "x vector must have length to match sigma matrix"
   num_dds = x.shape[0]
-  cdef np.ndarray[np.double_t, ndim=1, mode="c"] x_ = np.array(x, dtype=np.double)
-  cdef np.ndarray[np.double_t, ndim=2, mode="c"] sigma_ = np.array(sigma, dtype=np.double)
-  cdef np.ndarray[np.double_t, ndim=2, mode="c"] F_ = np.empty((num_dds,m), dtype=np.double)
-  cdef np.ndarray[np.double_t, ndim=1, mode="c"] s_ = np.empty((m), dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=1, mode="fortran"] x_ = np.array(x, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=2, mode="fortran"] sigma_ = np.array(sigma, dtype=np.double)
+  cdef np.ndarray[np.double_t, ndim=2, mode="fortran"] F_ = np.empty((num_dds,m), dtype=np.double, order='F')
+  cdef np.ndarray[np.double_t, ndim=1, mode="fortran"] s_ = np.empty((m), dtype=np.double, order='F')
   assert lambda_solution(num_dds, m, &x_[0], &sigma_[0,0], &F_[0,0], &s_[0]) == 0,"lambda error!"
   return (F_.T, s_.T)
