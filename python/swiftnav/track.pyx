533f804ce362b737059b67a35e0f821685826ec2
Jacob McNamee <jacob@swiftnav.com>
2016-05-12 15:01:40 -0700

    Pass receiver time delta into calc_navigation_measurement()

--- python/swiftnav/track.pyx
+++ python/swiftnav/track.pyx
@@ -546,7 +546,7 @@ cdef mk_nav_meas_array(py_nav_meas, u8 n_c_nav_meas, navigation_measurement_t *c
     memcpy(&c_nav_meas[i], &sd_, sizeof(navigation_measurement_t))
 
 # TODO (Buro): Remove mallocs, etc. here. Do all this in-place
-def _calc_navigation_measurement(chan_meas, nav_meas, nav_time, ephemerides):
+def _calc_navigation_measurement(chan_meas, nav_meas, ephemerides):
   """
   """
   n_channels = len(chan_meas)
@@ -558,7 +558,7 @@ def _calc_navigation_measurement(chan_meas, nav_meas, nav_time, ephemerides):
     chan_meas_[n] = &((<ChannelMeasurement ?>chan_meas[n])._thisptr)
     nav_meas_[n] = &((<NavigationMeasurement ?>nav_meas[n])._thisptr)
     ephs[n] = &((<Ephemeris ?>ephemerides[n])._thisptr)
-  calc_navigation_measurement(n_channels, chan_meas_, nav_meas_, nav_time, ephs)
+  calc_navigation_measurement(n_channels, chan_meas_, nav_meas_, ephs)
   free(chan_meas_)
   free(nav_meas_)
   free(ephs)
